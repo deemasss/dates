e926077e18bb4bee7f80b0d309db2ba33a6b29ea
deemasss <dimon.limon.s90@gmail.com>
2022-10-04 12:36:23 +0300

    Check

--- js/dates.js
+++ js/dates.js
@@ -40,7 +40,7 @@ function getDay(date, lang) {
 // Вид должен быть такой 12:02(часы и минуты), то есть если у вас одно число на одном из
 // компонентов, то добавляем 0 перед ним
 function formatTime(date) {
-  return (new Date(date)).toTimeString().slice(0, -50);
+  return date.toLocaleTimeString(toString).slice(0, -3)
   //getHours() getMinutes()
 }
 
